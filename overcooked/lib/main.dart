c5d393d23f2e4d55d7e3f912f3076ad6689c0106
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-07-21 12:06:07 +1000

    created basic recipe list

--- overcooked/lib/main.dart
+++ overcooked/lib/main.dart
@@ -1,111 +1,86 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+Future<Data> fetchRecipes() async {
+    final response = await http.get('https://overcooked.2brothers.tech/v1/recipes/at/0');
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+    if (response.statusCode == 200) {
+        return Data.fromJson(json.decode(response.body));
+    } else {
+        throw Exception('Failed to load post');
+    }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class Data {
+    final List<Recipe> recipes;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    Data({ this.recipes });
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+    factory Data.fromJson(Map<String, dynamic> json) {
+        var list = json['data']['recipes'] as List;
+        return Data(
+            recipes: list.map((i) => Recipe.fromJson(i)).toList()
+        );
+    }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Recipe {
+    final String title;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+    Recipe({ this.title });
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    factory Recipe.fromJson(Map<String, dynamic> json) {
+        return Recipe(
+            title: json['title']
+        );
+    }
+}
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Overcooked',
+            home: Scaffold(
+                appBar: AppBar(
+                    title: Text('Overcooked'),
+                ),
+                body: RecipeList(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        );
+    }
+}
+
+class RecipeListState extends State<RecipeList> {
+    @override
+    Widget build(BuildContext context) {
+        return FutureBuilder<Data>(
+            future: fetchRecipes(),
+            builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data.recipes.length,
+                        itemBuilder: (context, i) {
+                            return ListTile(
+                                title: Text(snapshot.data.recipes[i].title)
+                            );
+                        }
+                    );
+                } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                }
+                return CircularProgressIndicator();
+            }
+        );
+    }
+}
+
+class RecipeList extends StatefulWidget {
+    @override
+    RecipeListState createState() => RecipeListState();
 }
