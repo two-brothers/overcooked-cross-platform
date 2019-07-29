6346e4492deb1eae0a2dd78d047290793d7f3eed
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-07-29 18:54:59 +1000

    refactored lookup format

--- overcooked/lib/src/lookups/lookupIngredientUnitType.dart
+++ overcooked/lib/src/lookups/lookupIngredientUnitType.dart
@@ -1,20 +1,20 @@
 class LookupIngredientUnitType {
-    static const _None None = _None(-1, "", "");
-    static const _Singular Singular = _Singular(0, " ", " ");
-    static const _Grams Grams = _Grams(1, "g ", "g ");
-    static const _Millilitres Millilitres = _Millilitres(2, "ml ", "ml ");
-    static const _Tsp Tsp = _Tsp(3, " tsp ", " tsp ");
-    static const _Tbsp Tbsp = _Tbsp(4, " Tbsp ", " Tbsp ");
-    static const _Cups Cups = _Cups(5, " cup ", " cups ");
-    static const _Bunch Bunch = _Bunch(6, " bunch of ", " bunches of ");
-    static const _Rasher Rasher = _Rasher(7, " rasher of ", " rashes of ");
-    static const _Head Head = _Head(8, " head of ", " heads of ");
-    static const _Sprig Sprig = _Sprig(9, " sprig of ", " sprigs of ");
-    static const _Stalk Stalk = _Stalk(10, " stalk of ", " stalks of ");
-    static const _Sheet Sheet = _Sheet(11, " sheet of ", " sheets of ");
-    static const _Slice Slice = _Slice(12, " slice of ", " slices of ");
-    static const _Kilograms Kilograms = _Kilograms(101, "kg ", "kg ");
-    static const _Litres Litres = _Litres(201, "L ", "L ");
+    static const _None NONE = _None(-1, "", "");
+    static const _Singular SINGULAR = _Singular(0, " ", " ");
+    static const _Grams GRAMS = _Grams(1, "g ", "g ");
+    static const _Millilitres MILLILITERS = _Millilitres(2, "ml ", "ml ");
+    static const _Tsp TSP = _Tsp(3, " tsp ", " tsp ");
+    static const _Tbsp TBSP = _Tbsp(4, " Tbsp ", " Tbsp ");
+    static const _Cups CUPS = _Cups(5, " cup ", " cups ");
+    static const _Bunch BUNCH = _Bunch(6, " bunch of ", " bunches of ");
+    static const _Rasher RASHER = _Rasher(7, " rasher of ", " rashes of ");
+    static const _Head HEAD = _Head(8, " head of ", " heads of ");
+    static const _Sprig SPRIG = _Sprig(9, " sprig of ", " sprigs of ");
+    static const _Stalk STALK = _Stalk(10, " stalk of ", " stalks of ");
+    static const _Sheet SHEET = _Sheet(11, " sheet of ", " sheets of ");
+    static const _Slice SLICE = _Slice(12, " slice of ", " slices of ");
+    static const _Kilograms KILOGRAMS = _Kilograms(101, "kg ", "kg ");
+    static const _Litres LITERS = _Litres(201, "L ", "L ");
 }
 
 class _None {
