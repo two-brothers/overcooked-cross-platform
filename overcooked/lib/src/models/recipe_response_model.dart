acaa3c8daca1901d3d26a3fd2db324a195568cda
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-07-29 17:21:54 +1000

    added name and conversion data class

--- overcooked/lib/src/models/recipe_response_model.dart
+++ overcooked/lib/src/models/recipe_response_model.dart
@@ -36,16 +36,55 @@ class Data {
 }
 
 class Food {
-
     final String id;
+    final Name name;
+    final List<Conversion> conversions;
 
     Food({
-        this.id
+        this.id,
+        this.name,
+        this.conversions
     });
 
     factory Food.fromJson(Map<String, dynamic> json) {
         return Food(
             id: json['id'],
+            name: Name.fromJson(json['name']),
+            conversions: (json['conversions'] as List).map((i) => Conversion.fromJson(i)).toList()
+        );
+    }
+}
+
+class Name {
+    final String singular;
+    final String plural;
+
+    Name({
+        this.singular,
+        this.plural
+    });
+
+    factory Name.fromJson(Map<String, dynamic> json) {
+        return Name(
+            singular: json['singular'],
+            plural: json['plural']
+        );
+    }
+}
+
+class Conversion {
+    final double ratio;
+    final int unitId;
+
+    Conversion({
+        this.ratio,
+        this.unitId
+    });
+
+    factory Conversion.fromJson(Map<String, dynamic> json) {
+        return Conversion(
+            ratio: json['ratio'].toDouble(),
+            unitId: json['unitId']
         );
     }
 }
