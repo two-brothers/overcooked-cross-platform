4e747f443808056a258c0a94d3d47292f35010a6
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-08-10 15:58:22 +1000

    rendering styled recipe list layout

--- overcooked/lib/src/views/recipe_list_view.dart
+++ overcooked/lib/src/views/recipe_list_view.dart
@@ -44,7 +44,7 @@ class _RecipeListState extends State<RecipeList> {
               itemBuilder: (BuildContext context, int index) {
                 return index >= state.posts.length
                     ? BottomLoader()
-                    : PostWidget(post: state.posts[index]);
+                    : PostWidget(recipe: state.posts[index]);
               },
               itemCount: state.hasReachedMax
                   ? state.posts.length
@@ -57,72 +57,6 @@ class _RecipeListState extends State<RecipeList> {
           );
         },
       );
-        /* return FutureBuilder<RecipeListModel>(
-            future: Api.getRecipeList(),
-            builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                    return ListView.builder(
-                        itemCount: snapshot.data.recipes.length,
-                        padding: EdgeInsets.all(4),
-                        itemBuilder: (context, i) {
-                            final recipe = snapshot.data.recipes[i];
-                            return GestureDetector(
-                                onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => RecipeView(id: recipe.id)),
-                                    );
-                                },
-                                child: new Card(
-                                    child: Stack(
-                                        children: <Widget>[
-                                            Image.network("https://overcooked.2brothers.tech/${recipe.imageUrl}"),
-                                            Positioned(
-                                                bottom: 0,
-                                                left: 0,
-                                                width: MediaQuery.of(context).size.width,
-                                                height: 80,
-                                                child: (
-                                                    Container(
-                                                        decoration: BoxDecoration(
-                                                            gradient: LinearGradient(
-                                                                begin: Alignment.bottomCenter,
-                                                                end: Alignment.topCenter,
-                                                                colors: [Colors.black87, Colors.transparent]
-                                                            )
-                                                        ),
-                                                        child: Padding(
-                                                            padding: EdgeInsets.all(100),
-                                                        )
-                                                    )
-                                                )
-                                            ),
-                                            Positioned(
-                                                bottom: 16,
-                                                left: 16,
-                                                width: MediaQuery.of(context).size.width,
-                                                child: (
-                                                    Text(
-                                                        recipe.title.toUpperCase(),
-                                                        style: new TextStyle(
-                                                            fontSize: 16.0,
-                                                            color: Color(0xFFFFFFFF)
-                                                        ),
-                                                    )
-                                                )
-                                            )
-                                        ]
-                                    )
-                                )
-                            );
-                        }
-                    );
-                } else if (snapshot.hasError) {
-                    return Text("${snapshot.error}");
-                }
-                return CircularProgressIndicator();
-            }
-        );*/
     }
 
     @override
@@ -159,15 +93,60 @@ class BottomLoader extends StatelessWidget {
 }
 
 class PostWidget extends StatelessWidget {
-  final RecipeModel post;
+    final RecipeModel recipe;
 
-  const PostWidget({Key key, @required this.post}) : super(key: key);
+    const PostWidget({Key key, @required this.recipe}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.only(bottom: 60),
-        child: Text(post.title),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => RecipeView(id: recipe.id)),
+              );
+            },
+            child: new Card(
+                child: Stack(
+                    children: <Widget>[
+                        Image.network("https://overcooked.2brothers.tech/${recipe.imageUrl}"),
+                        Positioned(
+                            bottom: 0,
+                            left: 0,
+                            width: MediaQuery.of(context).size.width,
+                            height: 80,
+                            child: (
+                                Container(
+                                    decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                            begin: Alignment.bottomCenter,
+                                            end: Alignment.topCenter,
+                                            colors: [Colors.black87, Colors.transparent]
+                                        )
+                                    ),
+                                    child: Padding(
+                                      padding: EdgeInsets.all(100),
+                                    )
+                                )
+                            )
+                        ),
+                        Positioned(
+                            bottom: 16,
+                            left: 16,
+                            width: MediaQuery.of(context).size.width,
+                            child: (
+                                Text(
+                                  recipe.title.toUpperCase(),
+                                  style: new TextStyle(
+                                      fontSize: 16.0,
+                                      color: Color(0xFFFFFFFF)
+                                  ),
+                                )
+                            )
+                        )
+                    ]
+                )
+            )
+        );
+    }
 }
